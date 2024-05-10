2fbe9b5ae47909314478b3fa5a817dc9401b80c5
Nicolas Antonioli <nicolas_antonioli@microgestion.com>
2024-05-10 17:58:27 -0300

    Agregando proyecto base

---
+++ src/juego/Juego.java
@@ -0,0 +1,47 @@
+package juego;
+
+
+import entorno.Entorno;
+import entorno.InterfaceJuego;
+
+public class Juego extends InterfaceJuego
+{
+	// El objeto Entorno que controla el tiempo y otros
+	private Entorno entorno;
+	
+	// Variables y métodos propios de cada grupo
+	// ...
+	
+	Juego()
+	{
+		// Inicializa el objeto entorno
+		this.entorno = new Entorno(this, "Attack on Titan, Final Season - Grupo ... - v1", 800, 600);
+		
+		// Inicializar lo que haga falta para el juego
+		// ...
+
+		// Inicia el juego!
+		this.entorno.iniciar();
+	}
+
+	/**
+	 * Durante el juego, el método tick() será ejecutado en cada instante y 
+	 * por lo tanto es el método más importante de esta clase. Aquí se debe 
+	 * actualizar el estado interno del juego para simular el paso del tiempo 
+	 * (ver el enunciado del TP para mayor detalle).
+	 */
+	public void tick()
+	{
+		// Procesamiento de un instante de tiempo
+		// ...
+		
+
+	}
+	
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args)
+	{
+		Juego juego = new Juego();
+	}
+}
