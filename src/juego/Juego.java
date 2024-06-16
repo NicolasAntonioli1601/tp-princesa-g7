0a27cd79e16c74aed782d22cb906ed134e9931c0
Fabricio Cortez <fabricioj.cortez@gmail.com>
2024-06-15 23:26:50 -0300

    dino cae

--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -80,8 +80,10 @@ public class Juego extends InterfaceJuego {
 				this.dinos[dino] = new Tiranosaurio(530, 390, 50, 50,false,true);
 				//tiro[tiros]	= new TiroDino (530, 390, false);
 			if(dino == 5)
-				this.dinos[dino] = new Tiranosaurio(200 , 390, 50, 50,true);
-				//tiro[tiros]= new TiroDino (200 , 390, true);	
+				this.dinos[dino] = new Tiranosaurio(200 , 390, 50, 50,true,true);
+				//tiro[tiros]= new TiroDino (200 , 390, true);
+			
+				
 			}
 		this.princesa = new Princesa(400, 540, 50, 50);
 		//cubo.Colision(princesa,Fila1Fijo ,Fila1Random );
@@ -110,8 +112,11 @@ public class Juego extends InterfaceJuego {
 		Fila.dibujarFilas(this.filas, entorno);
 	
 		
+<<<<<<< HEAD
 			
 		//System.out.println(tiro[0].getX());
+=======
+>>>>>>> e03da0b (dino cae)
 		
 
 
@@ -180,6 +185,7 @@ public class Juego extends InterfaceJuego {
 
 		}
 		
+<<<<<<< HEAD
 		for(int x = 0; x < filas.length; x++) {
 			for(Cubo cubo : filas[x].getCubos()) {
 				if(x < filas.length -1) {
@@ -187,6 +193,17 @@ public class Juego extends InterfaceJuego {
 						if(cubo.getX()-20 < this.princesa.getX() && cubo.getX() < this.princesa.getX()+20) {
 							this.princesa.setPiso(filas[x].getCoordenadas().getY());
 						}
+=======
+		for(Fila fila : this.filas) {
+			for(Cubo cubo : fila.getCubos()) {
+				
+				if (this.princesa.getY() > cubo.getY() + 25){
+					if(!cubo.isRompible()) {
+						princesa.setPiso((int) cubo.getY());
+					}
+					else {
+						princesa.setPiso((int) cubo.getY()-150);
+>>>>>>> e03da0b (dino cae)
 					}
 				}
 				
